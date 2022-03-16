bdc61f9045f30bbce3ba74a39370784af5261df8
苏飞 <fei.su@imcore.com>
2022-03-16 18:31:06 +0800

    particle system

---
+++ Assets/Demo/IK/Scripts/TestIK.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestIK : MonoBehaviour
+{
+    private Animator anim;
+    public bool ikHeadActive;
+    public bool ikRightHandActive;
+    public Transform rightHandPosReference;
+    public Transform rightHandRotationReference;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetTrigger("Attack");
+        }
+    }
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+        if (!anim)
+        {
+            return;
+        }
+
+        if (ikHeadActive)
+        {
+            //鼠标位置发出的射线
+            Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Plane plane = new Plane(Vector3.forward, transform.position);
+            float rayDistance;
+            Vector3 target = Vector3.zero;
+            if (plane.Raycast(cameraRay, out rayDistance))
+            {
+                //获取射线与平面交叉的点
+                target = cameraRay.GetPoint(rayDistance);
+            }
+
+            anim.SetLookAtPosition(target);
+            anim.SetLookAtWeight(1);
+        }
+
+        if (ikRightHandActive)
+        {
+            anim.SetIKPositionWeight(AvatarIKGoal.RightHand, 1f);
+            anim.SetIKRotationWeight(AvatarIKGoal.RightHand, 1f);
+
+            anim.SetIKPosition(AvatarIKGoal.RightHand, rightHandPosReference.position);
+            anim.SetIKRotation(AvatarIKGoal.RightHand, rightHandRotationReference.rotation);
+        }
+    }
+}
---
+++ Assets/Demo/Particle/Scripts/CandleFlareTestUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandleFlareTestUI : MonoBehaviour
+{
+    public CandleFlare flare1;
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void OnGUI() {
+        if (GUI.Button(new Rect(10, 30, 50, 30), "东风")) {
+            flare1.increaseWindPower();
+        }
+
+        if (GUI.Button(new Rect(10, 70, 50, 30), "西风")) {
+            flare1.decreaseWindPower();
+        }
+
+        if (GUI.Button(new Rect(10, 110, 50, 30), "增强")) {
+            flare1.increaseFlame();
+        }
+
+        if (GUI.Button(new Rect(10, 150, 50, 30), "减弱")) {
+            flare1.decreaseFlame();
+        }
+    }
+}
